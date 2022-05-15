4e8d8ec666b734ebc20ee6e538933e77b13232c3
Rongbing Lai <rongbil1@uci.edu>
2022-05-15 16:49:00 -0700

    created api.java and product class

---
+++ justplants/src/main/java/com/justplants/ProductClient.java
@@ -0,0 +1,32 @@
+package com.justplants;
+
+public class ProductClient {
+
+    String name;
+    String id;
+    String rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
