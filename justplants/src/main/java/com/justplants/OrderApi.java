3bd90e151b37d114fe7d59a8e9d742caf4ad6794
Rongbing Lai <rongbil1@uci.edu>
2022-05-16 15:20:38 -0700

    addOrder api

--- justplants/src/main/java/com/justplants/OrderApi.java
+++ justplants/src/main/java/com/justplants/OrderApi.java
@@ -16,22 +16,21 @@ public class OrderApi {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getOrder(@PathParam("id") String id) throws SQLException {
         DatabaseHelper databaseHelper = new DatabaseHelper();
-        Connection con = databaseHelper.getConnection();            
+        Connection con = databaseHelper.getConnection();
         Statement stmt = con.createStatement();
-        String sql = "SELECT u_id FROM "+ databaseHelper.getOrder() +" WHERE u_id=" + id;
+        String sql = "SELECT id FROM " + databaseHelper.getOrder() + " WHERE id=" + id;
         ResultSet rs = stmt.executeQuery(sql);
 
         Order order = new Order();
         order.setId(id);
 
-        if (!rs.next()){
+        if (!rs.next()) {
             return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        else {
-            while (rs.next()){
+        } else {
+            while (rs.next()) {
                 order.setShipping(rs.getString("shipping"));
                 order.setUid(rs.getInt("u_id"));
-                for (int i = 1; i < 11; i++){
+                for (int i = 1; i < 11; i++) {
                     if (rs.getInt("p_" + i) == 1)
                         order.addOrder("p_" + i);
                 }
@@ -42,19 +41,42 @@ public class OrderApi {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addOrder(Order order)  {
-        
+    public Response addOrder(Order order) throws SQLException {
+        try {
+            DatabaseHelper databaseHelper = new DatabaseHelper();
+            Connection con = databaseHelper.getConnection();
+            Statement stmt = con.createStatement();
+            String sql = "INSERT INTO " + databaseHelper.getOrder() + "VALUES";
+            sql += "(" + order.getUid() + "," + order.getShipping() + ",";
+            for (int i = 1; i < 11; i++) {
+                if (i < 10) {
+                    if (order.getOrders().contains("p_" + i))
+                        sql += "1, ";
+                    else
+                        sql += "0, ";
+                } else {
+                    if (order.getOrders().contains("p_" + i))
+                        sql += "1)";
+                    else
+                        sql += "0)";
+                }
+            }
 
-        // System.out.println("Id: " + product.getId());
-        // System.out.println("name: " + product.getName());
-        // System.out.println("rating: " + product.getRating());
+            ResultSet rs = stmt.executeQuery(sql);
 
-        // add product to DB
-        if(isSuccess) {
-            return Response.ok().entity("Product Added Successfully").build();
-        }
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+            System.out.println("Id: " + order.getId());
+            System.out.println("Shipping: " + order.getShipping());
+            System.out.println("Uid: " + order.getUid());
+            System.out.println("Orders: ");
+            for (int i = 0; i < order.getOrders().size(); i++) {
+                System.out.println(i);
+            }
+
+            return Response.ok().entity("Order Added Successfully").build();
+
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }  
     }
 
-    
 }
